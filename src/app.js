90b7061837107c19a2e66d2f73f07b6f169278de
Rohan <ghadagerohan07@gmail.com>
2025-08-10 14:44:12 +0530

    added frontend link

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://job-portal-frontend-iota-eight.vercel.app/",
+      "https://job-portal-frontend-iota-eight.vercel.app",
       
       "https://job-portal-backend-kiot.onrender.com",
     ],
